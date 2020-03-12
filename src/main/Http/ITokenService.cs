968355b91d0fab488236d9dcfc5437cfde7db5d5
Elmer Roderick Bool <elmer.bool@ei8.works>
2020-03-13 00:14:26 +1100

    Added Http classes.

---
+++ src/main/Http/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace org.neurul.Common.Http
+{
+    public interface ITokenService
+    {
+        string GetAccessToken();
+    }
+}
